fa033fe24faf85ccf703baa08b9d695632c7cbec
Nikita.Chernyak <mynewacc.create@gmail.com>
2019-11-07 22:18:13 +0300

    Определены поля класса, добавлены методы для рассчета по формуле 1 и формуле 2

--- MainFrame.java
+++ MainFrame.java
@@ -18,7 +18,49 @@ import javax.swing.JTextField;
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame {
 
+	private static final int WIDTH = 600;
+	private static final int HEIGHT = 420;
 	
+	//Текстовые поля для считывания значений переменных 
+	private JTextField textFieldX;
+	private JTextField textFieldY;
+	private JTextField textFieldZ;
+	
+	//для вывода значений переменных
+	private JTextField textFieldResult;
+	private JTextField textFieldMemory;
+	
+	//Группы радио-кнопок для обеспечения уникальности выделения в группе
+	private ButtonGroup radioButtonsForFormula = new ButtonGroup();
+	private ButtonGroup radioButtonsForMemory = new ButtonGroup();
+	
+	//Контейнеры для отображения радио-кнопок
+	private Box hboxFormulaType = Box.createHorizontalBox();
+	private Box hboxMemoryType = Box.createHorizontalBox();
+	
+	private int formulaID = 1;
+	private int memID = 1;
+	private Double mem1 = 0.0, mem2 = 0.0, mem3 = 0.0;
+	
+	//Вычисление по формуле 1
+	public Double calculate_formula1(Double x, Double y, Double z) {
+		if (x == 0.0) {
+			JOptionPane.showMessageDialog(MainFrame.this, "'x' не должен равняться нулю", "Ошибка ввода", JOptionPane.WARNING_MESSAGE);
+			return 0.0;
+		}
+		return (Math.pow(Math.cos(Math.exp(y)) + Math.exp(Math.pow(y, 2)) + Math.sqrt(1/x), 0.25)) / 
+				(Math.pow(Math.cos(Math.PI * Math.pow(z, 3)) + Math.log(Math.pow(1 + z, 2)), Math.sin(y)));
+	}
+		
+	//Вычисление по формуле 2
+	public Double calculate_formula2(Double x, Double y, Double z) {
+		if (y == 0.0) {
+			JOptionPane.showMessageDialog(MainFrame.this, "'y' не должен равняться нулю", "Ошибка ввода", JOptionPane.WARNING_MESSAGE);
+			return 0.0;
+		}
+		return (Math.pow((1 + Math.pow(x, 2)), (1/y))) / (Math.exp(Math.sin(z) + x));
+	}
+		
 	public static void main(String[] args) {
 		MainFrame frame = new MainFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
